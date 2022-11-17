d52481f77eefbd869b346a9ea6c85e42871336c1
Juand30 <juandp2290@gmail.com>
2022-11-17 12:35:52 +0000

    act

--- src/js/component/Form.js
+++ src/js/component/Form.js
@@ -3,10 +3,7 @@ import React from "react";
 export function Form(props){
     const {tarea, handleSubmit, handleChange} = props
 
-    const keyDown =(event)=>{
-        if (event.key === 'Enter'){
-          console.log("has dado enter");
-        }
+    
     return (
         <form onSubmit={handleSubmit}>
             <input
@@ -14,11 +11,7 @@ export function Form(props){
                 placeholder="introduce la tarea" 
                 onChange={handleChange}
                 value={tarea}/>
-            <input
-                type="submit"
-                className="btn"
-                value="AGREGAR"
-                onClick={handleSubmit}/>    
+             
         </form>
     )
 
@@ -26,12 +19,3 @@ export function Form(props){
 
 export default Form;
 
-
-const keyDown =(event)=>{
-    if (event.key === 'Enter'){
-      console.log("has dado enter");
-    }
-      
-  }
-  return(
-    <input type="text" onChange={(event)=>setInput(event.target.value)} onKeyDown={keyDown}/>
