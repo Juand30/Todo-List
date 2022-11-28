48519b65a38d2070472aeba6a5ef3efb487b1ca9
Juand30 <juandp2290@gmail.com>
2022-11-28 17:00:16 +0000

    Actualizacion

--- src/js/component/Tarea.js
+++ src/js/component/Tarea.js
@@ -4,13 +4,19 @@ export function Tarea(props){
 
     const {tarea, onBorrarTarea} = props
 
- 
+    /*const [butonAct, setButoonAct] = useState (false);    
+
+    if (boton.onmouseover){
+        ButoonAct=(true);
+    }else{
+        ButoonAct=(false);    
+    }*/
 
         return (
             <>
                 <div id={tarea.id}>
                     <span>{tarea.tarea}</span>
-                    <button className="btn"
+                    <button id="boton" className="btn" disabled={!butonAct}
                     onClick={()=> onBorrarTarea(tarea.id)}>
                        <i class="fas fa-trash-alt"></i>
                     </button>
