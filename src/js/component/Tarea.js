0b8903febcc7195da829ebcccb503a4a46e3592a
Juand30 <juandp2290@gmail.com>
2023-04-15 12:49:54 +0000

    end

--- src/js/component/Tarea.js
+++ src/js/component/Tarea.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 
 export function Tarea(props){
 
+    const [boton, setBoton] = useState()
     const {tarea, onBorrarTarea} = props
 
         return (
             <>
-                <div id={tarea.id}>
+                <div id={tarea.id} onMouseOver={()=>setBoton(true)} onMouseLeave={()=>setBoton(false)}>  
                     <span>{tarea.tarea}</span>
                     <button id="boton" className="btn boton-oculto" 
                     onClick={()=> onBorrarTarea(tarea.id)}>
-                       <i class="fas fa-trash-alt"></i>
+                       <i class={`fas fa-trash-alt ${boton ? "" : "d-none" }`}></i>
                     </button>
                     
                 </div>
